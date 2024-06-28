cd05d3e13c603b9285262d944cc3e27e4b96acfb
Valery Semenenko <gearmobile@gmail.com>
2024-06-28 18:14:29 +0300

    feature: add model and data

---
+++ src/app/data/todo.data.ts
@@ -0,0 +1,11 @@
+import { ITodo, TodoModel } from '../model';
+
+export const TODOS: ITodo[] = [
+  new TodoModel('1', 'title 1', false),
+  new TodoModel('2', 'title 2', true),
+  new TodoModel('3', 'title 3', false),
+  new TodoModel('4', 'title 4', true),
+  new TodoModel('5', 'title 5', false),
+  new TodoModel('6', 'title 6', true),
+  new TodoModel('7', 'title 7', false),
+];
